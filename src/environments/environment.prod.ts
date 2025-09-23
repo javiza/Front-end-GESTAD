f10d9e0423007bdf9ae1962b818bb7b821b9424a
javiza <jonathan@gmail.com>
2025-09-23 14:33:58 -0300

    reparando errores de conexion con render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,3 @@ export const environment = {
   production: true,
   apiUrl: 'https://backe-end-gestad.onrender.com'
 };
-// export const environment = {
-//   production: true,
-//   apiUrl: '/api'
-// };
