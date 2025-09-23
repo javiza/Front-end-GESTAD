6b80387aa7df142875cb8e47a505039c71b26bc0
javiza <jonathan@gmail.com>
2025-09-23 13:16:36 -0300

    prueba render.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://backe-end-gestad.onrender.com'
-// };
 export const environment = {
   production: true,
-  apiUrl: '/api'
+  apiUrl: 'https://backe-end-gestad.onrender.com'
 };
+// export const environment = {
+//   production: true,
+//   apiUrl: '/api'
+// };
